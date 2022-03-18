227d6d83446825011b3fb0522d269c6ab4529333
fjl <18618166564@163.com>
2022-03-18 15:41:33 +0800

    对称的双端diff

--- 1.js
+++ 1.js
@@ -22,27 +22,67 @@ function patchKeyedChildren(n1, n2, container) {
   let newStartVNode = newChildren[newStartIdx];
   let newEndVNode = newChildren[newEndIdx];
 
-  if (oldStartVNode.key === newStartVNode.key) {
-    // 第一步
-  } else if (oldEndVNode.key === newEndVNode.key) {
-    // 第二步
-  } else if (oldStartVNode.key === newEndVNode.key) {
-    // 第三步
-  } else if (oldEndVNode.key === newStartVNode.key) {
-    // 第四步
-    /**
-     * 1 patch 节点打补丁
-     * 2 移动
-     * 3 更新索引值
-     */
-    // 1
-    patch(oldEndVNode, newStartVNode, container);
-    // 2
-    insert(oldEndVNode.el, container, oldStartVNode.el);
-    // 3
-    oldEndIdx--;
-    newStartIdx++;
-    oldEndVNode = oldChildren[oldEndIdx];
-    newStartVNode = newChildren[newStartIdx];
+  while (newStartIdx <= newEndIdx && oldStartIdx <= oldEndIdx) {
+    if (oldStartVNode.key === newStartVNode.key) {
+      // 第一步
+      /**
+       * 1 patch 节点打补丁
+       * 2 都是头节点，不需要移动
+       * 3 更新索引值
+       */
+      // 1
+      patch(oldStartVNode, newStartVNode, container);
+      // 3
+      oldStartIdx++;
+      newStartIdx++;
+      oldStartVNode = oldChildren[oldStartIdx];
+      newStartVNode = newChildren[newStartIdx];
+    } else if (oldEndVNode.key === newEndVNode.key) {
+      // 第二步
+      /**
+       * 1 patch 节点打补丁
+       * 2 都是尾节点，不需要移动位置
+       * 3 更新索引值
+       */
+      // 1
+      patch(oldEndVNode, newEndVNode, container);
+      // 3
+      oldEndIdx--;
+      newEndIdx--;
+      oldEndVNode = oldChildren[oldEndIdx];
+      newEndVNode = newChildren[newEndIdx];
+    } else if (oldStartVNode.key === newEndVNode.key) {
+      // 第三步
+      /**
+       * 1 patch 节点打补丁
+       * 2 移动位置 旧的头节点对应dom 移动到旧尾节点对应dom 的后面
+       * 更新索引值
+       */
+      // 1
+      patch(oldStartVNode, newEndVNode);
+      // 2
+      insert(oldEndVNode.el, container, oldEndVNode.el.nextSibling);
+      // 3
+      oldStartIdx++;
+      newEndIdx--;
+      oldStartVNode = oldChildren[oldStartIdx];
+      newEndVNode = newChildren[newStartIdx];
+    } else if (oldEndVNode.key === newStartVNode.key) {
+      // 第四步
+      /**
+       * 1 patch 节点打补丁
+       * 2 移动
+       * 3 更新索引值
+       */
+      // 1
+      patch(oldEndVNode, newStartVNode, container);
+      // 2
+      insert(oldEndVNode.el, container, oldStartVNode.el);
+      // 3
+      oldEndIdx--;
+      newStartIdx++;
+      oldEndVNode = oldChildren[oldEndIdx];
+      newStartVNode = newChildren[newStartIdx];
+    }
   }
 }
